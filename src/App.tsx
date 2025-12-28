5610bd8d66bf7e66124b5af497d257be0c19d8d5
Pratik <pmgajjar25@gmail.com>
2025-12-29 00:56:50 +0530

    style: backend.how/turbopuffer inspired design
    - Dark monospace theme (JetBrains Mono)
    - Gold/orange accent colors
    - Clean minimal layout
    - Footer with 'Made with ❤️ by Pratik'
    - Links to backend.how, GitHub, Twitter
    - Live at friend.backend.how

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { Landing } from './pages/Landing'
 import { CreateGroup } from './pages/CreateGroup'
 import { JoinGroup } from './pages/JoinGroup'
 import { ChallengeBoard } from './pages/ChallengeBoard'
+import { Footer } from './components/Footer'
 
 function App() {
+  const location = useLocation()
+  const showFooter = !location.pathname.startsWith('/room/')
+
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/create" element={<CreateGroup />} />
-      <Route path="/join/:code" element={<JoinGroup />} />
-      <Route path="/room/:code" element={<ChallengeBoard />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/create" element={<CreateGroup />} />
+        <Route path="/join/:code" element={<JoinGroup />} />
+        <Route path="/room/:code" element={<ChallengeBoard />} />
+      </Routes>
+      {showFooter && <Footer />}
+    </>
   )
 }
 
